3cabad367f0b35d5e694823dbf9e947aa52c7fb3
SynoCodeWeb <romanet70@yahoo.com>
2021-06-19 14:57:27 +0200

    First commit

---
+++ resizer/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace resizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCarica = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbLoaded = new System.Windows.Forms.ProgressBar();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCarica
+            // 
+            this.btnCarica.Location = new System.Drawing.Point(12, 56);
+            this.btnCarica.Name = "btnCarica";
+            this.btnCarica.Size = new System.Drawing.Size(264, 55);
+            this.btnCarica.TabIndex = 0;
+            this.btnCarica.Text = "CARICA";
+            this.btnCarica.UseVisualStyleBackColor = true;
+            this.btnCarica.Click += new System.EventHandler(this.btnCarica_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(12, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(264, 55);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ESPORTA";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 178);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(264, 55);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "OPZIONI";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "RESIZER";
+            // 
+            // pbLoaded
+            // 
+            this.pbLoaded.Location = new System.Drawing.Point(12, 267);
+            this.pbLoaded.Name = "pbLoaded";
+            this.pbLoaded.Size = new System.Drawing.Size(263, 51);
+            this.pbLoaded.TabIndex = 4;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(12, 247);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(65, 17);
+            this.lblProgress.TabIndex = 5;
+            this.lblProgress.Text = "LOADED";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(282, 330);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.pbLoaded);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnCarica);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCarica;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar pbLoaded;
+        private System.Windows.Forms.Label lblProgress;
+    }
+}
+
---
+++ resizer/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.IO;
+
+namespace resizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string[] files;
+        public static string exportDir;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pbLoaded.Increment(-100);
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    exportDir = fbd.SelectedPath;
+                    System.Windows.Forms.MessageBox.Show("Dir found: " + exportDir, "Message");
+                }
+            }
+                              //1920 //1080  
+            ResizeImage(0, 0, 256, 256);
+        }
+        public void ResizeImage(int X1, int Y1, int width, int height)
+        {
+            for (int i = 0; i < files.Length; i++)
+            {
+                string fileSaveName = exportDir + "\\" + Path.GetFileName(files[i]);
+                lblProgress.Text = files[i];
+                pbLoaded.Increment(100/files.Length);
+                using (Image image = Image.FromFile(files[i]))
+                {
+                    new Bitmap(image, width, height).Save(fileSaveName);
+                }
+            }
+            pbLoaded.Increment(100);
+            System.Windows.Forms.MessageBox.Show("Done!", "Message");
+
+        }
+
+        private void btnCarica_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    files = Directory.GetFiles(fbd.SelectedPath);
+                    System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                }
+                button2.Enabled = true;
+            }
+        }
+    }
+}
